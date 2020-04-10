2b31cf7cfe09279a6f5a86b64a72748c1f9c1295
Ray Alva <rayalva407@gmail.com>
2020-04-10 05:29:28 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,12 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != person
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+        
       raise PartnerError
     else
       person.partner = self
